3a5adcf51e28b71111de42e58c714ab2aa87409c
Lapecru64 <118864153+Lapecru64@users.noreply.github.com>
2023-07-07 20:07:54 -0600

    Entregable 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-
+import { useEffect, useState} from "react"
 import './App.css'
+import axios from "axios"
+import getRandomNumber from "./utils/getRandomNumber"
+import LocationInfo from "./components/LocationInfo"
+import ResidentCard from "./components/ResidentCard"
 
 function App() {
-  
+
+  const [ location, setLocation] = useState()
+
+  useEffect(() => {
+    const url = `https://rickandmortyapi.com/api/location/${getRandomNumber(126)}`
+    axios.get(url)
+    .then(res => setLocation(res.data))
+    .catch(err => console.error(err))
+
+  }, [])
+
 
   return (
     <>
       <div>
         <h1>RICK AND MORTY APP</h1>
+        <LocationInfo
+        location={location}
+        />
+        <div>
+          {
+          location?.residents.map(url => (
+          <ResidentCard
+            key={url}
+            url={url}
+            />
+          ))
+          }
+        </div>
       </div>
       
           </>
