b6ec4ba70d50f26c66a7d41ee86d6c418370193e
Lapecru64 <118864153+Lapecru64@users.noreply.github.com>
2023-07-08 21:28:18 -0600

    Entregable-2 terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,73 @@
-import { useEffect, useState} from "react"
-import './App.css'
-import axios from "axios"
-import getRandomNumber from "./utils/getRandomNumber"
-import LocationInfo from "./components/LocationInfo"
-import ResidentCard from "./components/ResidentCard"
+
+import { useEffect, useState } from "react";
+import './App.css';
+import axios from "axios";
+import getRandomNumber from "./utils/getRandomNumber";
+import LocationInfo from "./components/LocationInfo";
+import ResidentCard from "./components/ResidentCard";
+import FormLocation from "./components/FormLocation";
 
 function App() {
+  const [location, setLocation] = useState();
+  const [idLocation, setIdLocation] = useState(getRandomNumber(126));
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const [ location, setLocation] = 
-  useState()
-
-
+  // Añadimos el estado para manejar la página
+  const [page, setPage] = useState(1);
+  const itemsPerPage = 6;
 
   useEffect(() => {
-    const url = `https://rickandmortyapi.com/api/location/${getRandomNumber(126)}`
+    const url = `https://rickandmortyapi.com/api/location/${idLocation}`;
+    setIsLoading(true);
     axios.get(url)
-    .then(res => setLocation(res.data))
-    .catch(err => console.error(err))
-
-  }, [])
-
+      .then(res => {
+        setLocation(res.data);
+        setHasError(false);
+      })
+      .catch(err => {
+        console.error(err);
+        setHasError(true);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [idLocation]);
 
   return (
-    <>
-      <div>
-        <h1>RICK AND MORTY APP</h1>
-         
-        <LocationInfo
-        location={location}
-        />
-        <div className="resident-container">
-          {
-          location?.residents.map(url => (
-          <ResidentCard
-            key={url}
-            url={url}
-            />
-          ))
-          }
-        </div>
-      </div>
-      
-          </>
-  )
+    <div className="container">
+      <h1>RICK AND MORTY APP</h1>
+      <FormLocation setIdLocation={setIdLocation} />
+      {
+        isLoading 
+        ? (<h2>Loading...</h2>)
+        : (
+          hasError
+          ? (<h1>❌ Hey! you must provide an id from 1 to 126 😭</h1>)
+          : (
+            <>
+              <LocationInfo location={location} />
+              <div className="resident-container">
+                {
+                  location?.residents.slice((page-1)*itemsPerPage, page*itemsPerPage).map(url => (
+                    <ResidentCard
+                      key={url}
+                      url={url}
+                    />
+                  ))
+                }
+                <div className="pagination">
+                  <button onClick={() => setPage(prev => Math.max(prev - 1, 1))}>Prev</button>
+                  <span>Page {page}</span>
+                  <button onClick={() => setPage(prev => (location?.residents.length/itemsPerPage > prev ? prev + 1 : prev))}>Next</button>
+                </div>
+              </div>
+            </>
+          )     
+        )
+      }
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/FormLocation.jsx
@@ -0,0 +1,27 @@
+
+import getRandomNumber from "../utils/getRandomNumber"
+
+const FormLocation = ({ setIdLocation }) => {
+
+const handleSubmit = e => {
+e.preventDefault()
+let inputValue = e.target.inputId.value.trim()
+
+// Asegurar que el valor ingresado esté en el rango correcto
+if (inputValue === '' || inputValue === "0" || inputValue < 1 || inputValue < 126){
+  setIdLocation(getRandomNumber(126))
+}else {
+  setIdLocation(inputValue)
+}
+e.target.inputId.value = '' 
+} 
+
+  return (
+    <form onSubmit={handleSubmit}>
+    <input id="inputId" className="input" style={{ bosShadow: '1px 1px 10px'}}type="text" />
+    <button style={{ backgroundColor: 'green', color: 'white'}}>Search</button>
+   </form>
+  )
+}
+
+export default FormLocation
